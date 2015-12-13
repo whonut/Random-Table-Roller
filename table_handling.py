ab2e4b24a08489e1e9375cc1fedc42ec789c3431
whonut <dylan@physicynicism.com>
2015-12-13 14:39:16 +0000

    Consolidate table_loader.py & autoroller.py
    Merge table_loader.py and autoroller.py into table_handling.py.
    The intent is that table loading and rolling functionality will be kept
    as separate as possible from the GUI code.

--- table_handling.py
+++ table_handling.py
@@ -1,4 +1,5 @@
 import csv
+from random import randint
 from itertools import chain
 
 
@@ -48,3 +49,24 @@ def load_table(filepath, headers=False):
         raise IOError('There is a gap in the table "{}"'.format(filepath))
 
     return table
+
+
+def roll_against(table):
+    '''Return a randomly chosen entry (a dict value) from a roll table.
+
+    table must be a dict of the same format as those created by load_table.'''
+
+    # Get the range of rolls that the table caters for.
+    permitted = list(chain(*table.keys()))
+    max_permitted = max(permitted)
+    min_permitted = min(permitted)
+
+    # Generated a random integer bounded by the maximum and minimum permitted
+    # rolls.
+    roll = randint(min_permitted, max_permitted)
+
+    # Check which event the roll corresponds to and return the description of
+    # that event.
+    for entry in table.items():
+        if roll in entry[0]:
+            return entry[1]
