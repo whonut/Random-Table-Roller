a6a0076a4e078035b3da283ef60c541c49b98551
whonut <dylan@physicynicism.com>
2015-12-13 12:03:38 +0000

    Fixed indentation on load_table docstring

--- table_loader.py
+++ table_loader.py
@@ -2,20 +2,20 @@ import csv
 
 
 def load_table(filepath, headers=False):
-    ''' Return a dict representing a roll table loaded from filepath.
+    '''Return a dict representing a roll table loaded from filepath.
 
-        Loads a roll table from the CSV file at filepath into a dict whose keys
-        are tuples specifying the range of rolls (min, max) associated with the
-        event specified in that key's value (a string describing the event).
-        If headers is True, then it is assumed that the first row of the file
-        contains some descriptive headers and the row is ignored. It defaults
-        to False.
+    Loads a roll table from the CSV file at filepath into a dict whose keys
+    are tuples specifying the range of rolls (min, max) associated with the
+    event specified in that key's value (a string describing the event).
+    If headers is True, then it is assumed that the first row of the file
+    contains some descriptive headers and the row is ignored. It defaults
+    to False.
 
-        The first column of the CSV should be the numbers or ranges of numbers
-        to roll in order to 'bring about' the associated event in the same row
-        of the second column. Ranges should be specified with dashes e.g.
-        a roll of 1 to 10 inclusive would be written as '1-10'. None of the
-        intervals should overlap.'''
+    The first column of the CSV should be the numbers or ranges of numbers
+    to roll in order to 'bring about' the associated event in the same row
+    of the second column. Ranges should be specified with dashes e.g.
+    a roll of 1 to 10 inclusive would be written as '1-10'. None of the
+    intervals should overlap.'''
 
     table = {}
     with open(filepath, newline='') as table_file:
