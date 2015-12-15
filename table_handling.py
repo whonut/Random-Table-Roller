4049d73f97608804b4ae6ce05ec983489d2f0873
whonut <dylan@physicynicism.com>
2015-12-15 19:17:00 +0000

    Remove headers option from load_table
    As mentioned in 5592556, the GUI for the table roller has no way of
    letting the user specify whether a file's first line is a table entry or
    merely a header line. No satisfactory way to allow the user to specify
    this can be found, so the headers option has been removed from
    load_table and headers are now a mandatory part of the format for CSV
    table files.

--- table_handling.py
+++ table_handling.py
@@ -3,15 +3,14 @@ from random import randint
 from itertools import chain
 
 
-def load_table(filepath, headers=False):
+def load_table(filepath):
     '''Return a dict representing a roll table loaded from filepath.
 
     Loads a roll table from the CSV file at filepath into a dict whose keys
     are ranges containing the range of rolls (min, max) associated with the
     event specified in that key's value (a string describing the event).
-    If headers is True, then it is assumed that the first row of the file
-    contains some descriptive headers and the row is ignored. It defaults
-    to False.
+    It is assumed that the first row of the file contains some descriptive
+    headers e.g. "Roll,Event" and the row is ignored.
 
     The first column of the CSV should be the numbers or ranges of numbers
     to roll in order to 'bring about' the associated event in the same row
@@ -25,8 +24,8 @@ def load_table(filepath, headers=False):
     with open(filepath, newline='') as table_file:
         table_reader = csv.reader(table_file)
         for row in table_reader:
-            if headers and table_reader.line_num == 1:
-                # Ignore the first line if headers is True
+            if table_reader.line_num == 1:
+                # Ignore the first line.
                 continue
             roll = row[0]
             event = row[1]
