db9ff3d592a80aa36ed6a0f8d9988d89bc60a830
whonut <dylan@physicynicism.com>
2015-12-15 20:39:23 +0000

    Remove try, except in load_table
    csv.Error is no longer caught in load_table, so that it may be handled
    directly by the GUI.

--- table_handling.py
+++ table_handling.py
@@ -30,12 +30,7 @@ def load_table(filepath):
 
     table = {}
     with open(filepath, newline='') as table_file:
-        try:
-            table_reader = csv.reader(table_file, strict=True)
-        except csv.Error as err:
-            # Bad CSV input.
-            raise TableFormatError("Bad CSV input") from err
-
+        table_reader = csv.reader(table_file, strict=True)
         for row in table_reader:
             if len(row) != 2:
                 # Tables should only have two columns.
